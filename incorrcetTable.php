99fbd6752553ea35928d44437471e05cb367e39a
segevchai <segevhai@gmail.com>
2020-03-30 21:04:29 +0300

    incorretTable + php

--- incorrcetTable.php
+++ incorrcetTable.php
@@ -1,3 +1,19 @@
+
+<?php 
+
+$conn = mysqli_connect('localhost','root','','test_project');
+
+if (!$conn) {
+	echo "failed";
+
+}
+
+$query="select * from faulty";
+$result = mysqli_query($conn , $query);
+?>
+
+
+
 <html lang="en">
   <head>
    
@@ -212,7 +228,7 @@
                 <span class="navbar-toggler-icon"></span>
                 </button>
                     <a class="logout1" style="right-side: 5px">
-                    <button class="btn btn-dark" onclick = "location.href='LoginMD.html';" id="logout" type="submit" >Log Out</button>
+                    <button class="btn btn-dark" onclick = "location.href='file:///C:/Users/Asus/Desktop/machinesAndDesc/LoginMD.html';" id="logout" type="submit" >Log Out</button>
 					</a>
             </nav>
 
@@ -220,16 +236,16 @@
          
 				<div id="mySidenav" class="sidenav">
 					   <div class="column" id="backPage1">
-                         <button class="btn btn-light" style="text-align:center;" onclick = "location.href='historyTable.html';" data-toggle="tooltip"  title="Machine Therapy History" id="History"><img src="http://www.up2me.co.il/imgs/77306062.png" alt="" style="width:"></button>
+                         <button class="btn btn-light" style="text-align:center;" onclick = "location.href='file:///C:/Users/Asus/Desktop/machinesAndDesc/historyTable.html';" data-toggle="tooltip"  title="Machine Therapy History" id="History"><img src="http://www.up2me.co.il/imgs/77306062.png" alt="" style="width:"></button>
                          </div>
                     <div class="column" id="backPage1">
-                        <button class="btn btn-light" style="text-align:center;" onclick = "location.href='incorrcetTable.html';"  data-toggle="tooltip" data-placement="bottom" title="Incorrect Components" id="Incorrect"><img src="http://www.up2me.co.il/imgs/46232313.png" alt="" style="width:"></button>
+                        <button class="btn btn-light" style="text-align:center;" onclick = "location.href='file:///C:/Users/Asus/Desktop/machinesAndDesc/incorrcetTable.html';"  data-toggle="tooltip" data-placement="bottom" title="Incorrect Components" id="Incorrect"><img src="http://www.up2me.co.il/imgs/46232313.png" alt="" style="width:"></button>
                          </div>
                     <div class="column" id="insert">
-                        <button class="btn btn-light" style="text-align:center;" onclick = "location.href='insertComponent.html';" data-toggle="tooltip" data-placement="bottom" title="Insert Components" id="Incorrect"><img src="http://www.up2me.co.il/imgs/14121216.png" alt="" style="width:"></button>
+                        <button class="btn btn-light" style="text-align:center;" onclick = "location.href='file:///C:/Users/Asus/Desktop/machinesAndDesc/insertComponent.html';" data-toggle="tooltip" data-placement="bottom" title="Insert Components" id="Incorrect"><img src="http://www.up2me.co.il/imgs/14121216.png" alt="" style="width:"></button>
                             </div>
                     <div class="column" id="insert">
-                       <button class="btn btn-light" style="text-align:center;"  onclick = "location.href='pageStockEnter.html';" data-toggle="tooltip" data-placement="bottom" title="Component Stock" id="ComponentPool"><img src="http://www.up2me.co.il/imgs/28542967.png" alt="" style="width:"></button>
+                       <button class="btn btn-light" style="text-align:center;"  onclick = "location.href='file:///C:/Users/Asus/Desktop/machinesAndDesc/pageStockEnter.html';" data-toggle="tooltip" data-placement="bottom" title="Component Stock" id="ComponentPool"><img src="http://www.up2me.co.il/imgs/28542967.png" alt="" style="width:"></button>
                           </div>
 						  
           </div>
@@ -246,7 +262,7 @@
                  <button class="btn btn-outline-dark" onclick = window.history.back() data-toggle="tooltip" data-placement="bottom" title="Back" id="backPage"><img src="http://www.up2me.co.il/imgs/18785403.png" alt="" style="width:" ></button>
                   </div>
                 <div class="column" id="home_Page">
-                 <button type="button" class="btn btn-outline-dark" onclick = "location.href='mapOfBilding.html';" data-toggle="tooltip" data-placement="bottom" title="Home Page" id="homePage"><img src="http://www.up2me.co.il/imgs/38159196.png" alt="" style="width:"></button>
+                 <button type="button" class="btn btn-outline-dark" onclick = "location.href='file:///C:/Users/Asus/Desktop/machinesAndDesc/mapOfBilding.html';" data-toggle="tooltip" data-placement="bottom" title="Home Page" id="homePage"><img src="http://www.up2me.co.il/imgs/38159196.png" alt="" style="width:"></button>
                   </div>      
                   <div class="column" id="Brint">  
                        <button class="btn btn-outline-dark" onclick = window.print()  data-toggle="tooltip" data-placement="bottom" title="Print" id="PrintPage"><img src="http://www.up2me.co.il/imgs/64977067.png" alt="" style="width:"></button>
@@ -276,51 +292,39 @@
                <table class="table table-hover" id="IncorrectTable">
                 <thead>
                   <tr>
-                    <th scope="col">Serial</th>
+                    <th scope="col">Fault Number</th>
                     <th scope="col">Component Type</th>
                     <th scope="col">ID Component</th>
                     <th scope="col">Cause Of Malfunction</th>
                     <th scope="col">Fault Description</th>
-                    <th scope="col">Fault Number</th>
+                    
                   </tr>
                 </thead>
+
+
+                <?php
+                     while($rows = mysqli_fetch_assoc($result)) {
+
+                ?>
+
                 <tbody>
                   <tr>
-                    <th scope="row">1</th>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                    <td>Bla Bla</td>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
+                    <th scope="row"><?php echo $rows['FaultNumber']; ?></th>
+                    <td><?php echo $rows['Component_type']; ?></td>
+                    <td><?php echo $rows['Component_ID']; ?></td>
+                    <td><?php echo $rows['CouseOfMalfunction']; ?></td>
+                    <td><?php echo $rows['FaultDescription']; ?></td>
+                    
                    
                   </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                    <td>Bla Bla</td>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                    <td>Bla Bla</td>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">4</th>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                    <td>Bla Bla</td>
-                    <td>Bla Bla</td>
-                    <td>elample</td>
-                  </tr>
-                  <tr>
+                 
                     
                 </tbody>
+
+                <?php
+                    }
+                ?>
+
               </table>
 
             </div>
