86bcd34e1ed789684b4d7f9c85973914779b0cda
NoamNehemya <noamnehemia8383@gmail.com>
2020-04-10 17:40:43 +0300

    Order by on support column doing casting on the column:
    SELECT DISTINCT support FROM machines ORDER BY cast(support as unsigned)

--- server.php
+++ server.php
@@ -360,7 +360,7 @@
             $query_list="SELECT DISTINCT Component_type FROM components";
             $result_list = mysqli_query($db , $query_list);
 
-            $query_list2="SELECT DISTINCT support FROM machines ORDER BY support DESC";
+            $query_list2="SELECT DISTINCT support FROM machines ORDER BY cast(support as unsigned)"; // need cast because doing order by on String column
             $result_list2 = mysqli_query($db , $query_list2);
 
           if(isset($_POST['insertComponentBtn'])) {
