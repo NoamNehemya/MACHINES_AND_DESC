3b552528a022fcd054b52f231944b09a606da24d
NoamNehemya <noamnehemia8383@gmail.com>
2020-05-03 16:16:10 +0300

    Add update cookie function, for dyncmic cookie after change component type from drop down list

--- vibration_machines.php
+++ vibration_machines.php
@@ -323,21 +323,21 @@
     <h3 id="title_Components_Modification" ><b>Components Modification</b></h3><br>
     
     <label><b>Component Type</b></label><br>
-    <select class="form-control" id="DropListType_one">
+    <select class="form-control" id="DropListType_one" onChange="updateCookie(DropListOneResult() )">
     <?php while($rows = $result_to_dropList_enter_stock->fetch_assoc()):;?>
     <?php echo "<option value={$rows["Component_type"]}>{$rows["Component_type"]}</option>";?>
     <?php endwhile;?>
     </select><br><br>
 
     <label><b>Add Component</b></label><br>
-    <select class="form-control" id="DropListType_two" onChange="submitActiveOrDimmed()">
+    <select class="form-control" id="DropListType_two" onChange="updateCookie(DropListOneResult() )">
     <?php while($rows = $result_query_two->fetch_assoc()):;?>
     <?php echo "<option value={$rows["s_n"]}>{$rows["s_n"]}</option>";?>
     <?php endwhile;?>
     </select><br><br>
     
     <label><b>Remove Component</b></label><br>
-    <select class="form-control" id="DropListType_three" onChange="submitActiveOrDimmed()">
+    <select class="form-control" id="DropListType_three" onChange="updateCookie(DropListOneResult() )">
     <?php while($rows = $result_query_three->fetch_assoc()):;?>
     <?php echo "<option value={$rows["s_n"]}>{$rows["s_n"]}</option>";?>
     <?php endwhile;?>
@@ -444,8 +444,10 @@ function closeNav() {
       return document.getElementById('DropListType_one').options[document.getElementById('DropListType_one').selectedIndex].innerHTML;
     }
 
-    
-   document.cookie = "headvalue="+DropListOneResult(); // store value of DropListOneResult in JS cookie
+   function updateCookie(x){
+    document.cookie = "headvalue="+x; // store value of DropListOneResult in JS cookie
+   } 
+   
  
 
 
