abeb5d7622c49a48fe66c9874e1b27efecfcd14a
NoamNehemya <noamnehemia8383@gmail.com>
2020-05-09 13:11:41 +0300

    Clean code - remove sideNav code from this page and import sideNav.php code

--- Buliding-six.php
+++ Buliding-six.php
@@ -24,65 +24,10 @@
 
   <head>
   
-  <link rel="stylesheet" href="SideNav.css">
   <link rel="stylesheet" href="Buliding-six.css">
   <link rel="stylesheet" href="AlertbuildingSix.css">
 
-
-                <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top" style="background-color:#669999; color: black; text-decoration-color: black;">
-				<div id="main">
-					<span id="navButton" style="font-size:30px;cursor:pointer">&#9776;</span>
-				</div>
-                <a class="navbar-brand" href="#">M&D</a>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-
-
-                    <a class="logout1" style="right-side: 5px">
-                      <button class="btn btn-dark" onclick = "location.href='LoginMD.php';" id="logout" type="submit" >Log Out</button>
-					</a>
-                
-            </nav>
-
-
-				<div id="mySidenav" class="sidenav">
-					     <div class="column" id="backPage1">
-                         <button class="btn btn-light" style="text-align:center;" onclick = "location.href='historyTable.php';" data-toggle="tooltip"  title="Machine Therapy History" id="History"><img src="http://www.up2me.co.il/imgs/77306062.png" alt="" style="width:"></button>
-                         </div>
-                    <div class="column" id="backPage1">
-                        <button class="btn btn-light" style="text-align:center;" onclick = "location.href='incorrcetTable.php';"  data-toggle="tooltip" data-placement="bottom" title="Incorrect Components" id="Incorrect"><img src="http://www.up2me.co.il/imgs/46232313.png" alt="" style="width:"></button>
-                         </div>
-                    <div class="column" id="insert">
-                        <button class="btn btn-light" style="text-align:center;" onclick = "location.href='insertComponent.php';" data-toggle="tooltip" data-placement="bottom" title="Insert Components" id="Incorrect"><img src="http://www.up2me.co.il/imgs/14121216.png" alt="" style="width:"></button>
-                            </div>
-                    <div class="column" id="insert">
-                       <button class="btn btn-light" style="text-align:center;"  onclick = "location.href='pageStockEnter.php';" data-toggle="tooltip" data-placement="bottom" title="Component Stock" id="ComponentPool"><img src="http://www.up2me.co.il/imgs/28542967.png" alt="" style="width:"></button>
-                          </div>
-						  
-          </div>
-          
-          <div class="jumbotron-jumbotron-fluid">
-            <div class="row">
-              <div class="column" id="titleMachine">
-                <h1 class="display-5" id="titleOFpage">Map - Building 6</h1>
-                </div>
-                <div class="column" id="Search_Machine">
-                <input type="text" class="form-control" placeholder="Search Machine:" id="SearchSupport" lt="" style="width:150">
-                </div>
-                <div class="column" id="back_Page">
-                 <button class="btn btn-outline-dark" onclick = window.history.back() data-toggle="tooltip" data-placement="bottom" title="Back" id="backPage"><img src="http://www.up2me.co.il/imgs/18785403.png" alt="" style="width:" ></button>
-                  </div>
-                <div class="column" id="home_Page">
-                 <button type="button" class="btn btn-outline-dark" onclick = "location.href='mapOfBilding.php';" data-toggle="tooltip" data-placement="bottom" title="Home Page" id="homePage"><img src="http://www.up2me.co.il/imgs/38159196.png" alt="" style="width:"></button>
-                  </div>      
-                  <div class="column" id="Brint">  
-                       <button class="btn btn-outline-dark" onclick = window.print()  data-toggle="tooltip" data-placement="bottom" title="Print" id="PrintPage"><img src="http://www.up2me.co.il/imgs/64977067.png" alt="" style="width:"></button>
-                    </div>
-                </div>
-				  
-  </head>
+  <?php include('sideNav.php');?>   	
   
   <body>
   
@@ -306,50 +251,6 @@
     <script src="myscripts.js"></script>
     <script>
 
-      document.getElementById("navButton").addEventListener("click", toggleNav);
-
-function toggleNav(){
-    if (document.getElementById("mySidenav").style.width === "0px" || document.getElementById("mySidenav").style.width === "") {
-        return openNav();
-    } else if (document.getElementById("mySidenav").style.width === "90px") {
-		return closeNav();
-	  }
-    
-}
-
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "90px";
-  document.getElementById("main").style.marginLeft = "90px";
-  document.getElementById("titleMachine").style.marginLeft = "150px";
-  document.getElementById("Search_Machine").style.marginLeft = "0px";
-  document.getElementById("back_Page").style.marginLeft = "0px";
-  document.getElementById("home_Page").style.marginLeft = "0px";
-  document.getElementById("Brint").style.marginLeft = "0px";
-  document.getElementById("explanMachinePage").style.marginLeft = "272px";
-
-  document.getElementById("titleMachine").style.transition = "0.5s";
-  document.getElementById("Search_Machine").style.transition = "0.5s";
-  document.getElementById("back_Page").style.transition = "0.5s";
-  document.getElementById("home_Page").style.transition = "0.5s";
-  document.getElementById("Brint").style.transition = "0.5s";
-  document.getElementById("explanMachinePage").style.transition = "0.5s";
-  
-}
-
-function closeNav() {
- document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.getElementById("titleMachine").style.marginLeft = "60px";
-  document.getElementById("explanMachinePage").style.marginright = "0px";
-  document.getElementById("Search_Machine").style.marginLeft = "0px";
-  document.getElementById("back_Page").style.marginRight = "0";
-  document.getElementById("home_Page").style.marginRight = "0";
-  document.getElementById("Brint").style.marginRight = "0";
-
-  document.getElementById("explanMachinePage").style.marginLeft = "180px";
- 
-}
 
 // This command save the result of machine ID (support) from array of same class buttons [0..n] and display the pressed machine in machine page (vibration\electro...)  
 
