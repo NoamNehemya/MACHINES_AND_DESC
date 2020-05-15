109a9995228aab6cca43b17448643acaceef1127
segevchai <segevhai@gmail.com>
2020-05-15 17:23:38 +0300

    insert comments

--- server_machine_error.php
+++ server_machine_error.php
@@ -1,17 +1,18 @@
 
-<?php
 
-$conn = mysqli_connect('localhost','root','','test_project');
+<?php//Creating this server page is meant to save us the code writing on one server page to create order
+
+$conn = mysqli_connect('localhost','root','','test_project');//New connection to the database
 
 if (!$conn) {
 	echo "failed";
 
 }
 
-$myPhpVar= $_COOKIE['myJavascriptVar_error'];
+$myPhpVar= $_COOKIE['myJavascriptVar_error']; //Creates a machine name page variable within the search bar if it is not found and returns it to a machine error page
 
-$myPhpVar_support= $_COOKIE['myJavascriptVar'];
+$myPhpVar_support= $_COOKIE['myJavascriptVar']; //Creating a Variable for Component Replacement Page This variable returns to the Component Replacement Page strp one and step two as the machine support
 
-$myPhpVar_type_Component= $_COOKIE['myJavascriptVar_type'];
+$myPhpVar_type_Component= $_COOKIE['myJavascriptVar_type']; //Creates a variable for the machine component type that a user wants to replace and returns to a step two in component replacement
 
 ?>
